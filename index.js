64efdb1481afe30ade11736e07928351ed399153
debanath <debanath26@gmail.com>
2022-10-11 22:45:25 +0530

    finally it respond to the ping command 🐢

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-// Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
-require('dotenv').config();
+import dotenv from 'dotenv'
+dotenv.config()
 
+import { Client, GatewayIntentBits } from 'discord.js'
 
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-
-client.once('ready', () => {
-	console.log('Ready!');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async(message) => {
+    // console.log(message);
+    if (message.content==='ping') {
+        message.channel.send('Pong!');
+    }
+});
 
 client.login(process.env.TOKEN);
