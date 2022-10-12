b1e7613b74de2fb7b1ba6c816c50bd43ec9954c8
debanath <debanath26@gmail.com>
2022-10-12 15:14:59 +0530

    addded respond on mention

--- index.js
+++ index.js
@@ -10,18 +10,35 @@ const client = new Client({
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.DirectMessages,
         GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages,
     ],
 });
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.username}!`);
 });
 
+
+// this respond to a message
 client.on('messageCreate', async(message) => {
-    // console.log(message);
+    console.log(message);
+    // console.log(message.content);
+    // console.log(message.guild.name);
+    // console.log(message.author.id);
+    // console.log(message.content);
+
+    // if the message contains mention to the bot
+    if (message.mentions.has(client.user)) {
+        message.channel.send(`Hello <@${message.author.id}> did you mention me? 😳`);
+    } 
+
+    // if(message.content. === '<@1029283042017169448>'){
     if (message.content==='ping') {
-        message.channel.send('Pong!');
+        message.channel.send('Pong! 🏓');
     }
+
 });
+
+
 
 client.login(process.env.TOKEN);
